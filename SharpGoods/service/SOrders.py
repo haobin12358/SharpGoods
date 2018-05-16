0fef41b7b8b7c1d55c08aadd74b789de4023c8f9
shuaibinliu <13588046135@163.com>
2018-05-16 23:20:23 +0800

    append fix

---
+++ SharpGoods/service/SOrders.py
@@ -0,0 +1,60 @@
+# *- coding:utf8 *-
+import sys
+import os
+sys.path.append(os.path.dirname(os.getcwd()))
+from SBase import SBase, close_session
+from models.model import OrderMain, Orderpart
+from models import model
+from common.TransformToList import trans_params
+
+
+class SOrders(SBase):
+    def __init__(self):
+        super(SOrders, self).__init__()
+
+    def get_uid_by_omid(self, omid):
+        uid = None
+        try:
+            uid = self.session.query(model.OrderMain.USid).filter_by(OMid=omid).scalar()
+        except Exception as e:
+            print e.message
+            self.session.rollback()
+        finally:
+            self.session.close()
+        return uid
+
+    def get_omstatus_by_omid(self, omid):
+        uid = None
+        try:
+            uid = self.session.query(model.OrderMain.OMstatus).filter_by(OMid=omid).scalar()
+        except Exception as e:
+            print e.message
+            self.session.rollback()
+        finally:
+            self.session.close()
+        return uid
+
+    def update_omstatus_by_omid(self, omid, order_main):
+        try:
+            self.session.query(model.OrderMain).filter_by(OMid=omid).update(order_main)
+            self.session.commit()
+            self.session.close()
+            return True
+        except Exception as e:
+            self.session.rollback()
+            self.session.close()
+            print e.message
+            return False
+
+    @close_session
+    def get_order_part_list_by_omid(self, omid):
+        return self.session.query(
+            Orderpart.OPid, Orderpart.PBid, Orderpart.PRnumber).filter(Orderpart.OMid == omid).all()
+
+    @close_session
+    def get_order_main_list_by_usid(self, usid):
+        return self.session.query(
+            OrderMain.OMid, OrderMain.LOid, OrderMain.COid,
+            OrderMain.OMabo, OrderMain.OMcointype, OrderMain.OMstatus,
+            OrderMain.OMtime, OrderMain.OMprice
+        ).all()
